491a53846e567c1e8dc92f9672561d395f30c4b7
xuchaoS <xuchaoshang@gmail.com>
2018-04-12 23:55:14 +0800

    添加测试环境管理

--- machines/models.py
+++ machines/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Machine(models.Model):
+    name = models.CharField(max_length=50)
+    ip = models.GenericIPAddressField()
+    system = models.CharField(max_length=50)
+    note = models.TextField(blank=True)
